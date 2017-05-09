697ef6948b849b5677d2982397e020b97649bc38
MH123HACKVPC\mh123hack <m@j.nl>
2017-05-10 01:57:24 +0200

    some more space made

--- md5/src/md5/Md5.java
+++ md5/src/md5/Md5.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
 /**
  *
